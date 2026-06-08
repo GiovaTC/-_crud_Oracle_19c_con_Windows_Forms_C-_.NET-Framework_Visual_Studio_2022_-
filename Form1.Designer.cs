d344f0714999fad71f90173a8d73a8eecf82261f
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-08 10:48:01 -0500

    commit - Form1 / .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,37 +3,205 @@
     partial class Form1
     {
         /// <summary>
-        ///  Required designer variable.
+        /// Variable del diseñador requerida.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblTelefono;
+
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefono;
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+
+        private System.Windows.Forms.DataGridView dgvClientes;
+
         /// <summary>
-        ///  Clean up any resources being used.
+        /// Limpiar recursos.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing"></param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
             {
                 components.Dispose();
             }
+
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblTelefono = new System.Windows.Forms.Label();
+
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+
+            this.dgvClientes = new System.Windows.Forms.DataGridView();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).BeginInit();
+
+            this.SuspendLayout();
+
+            // lblId
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(30, 30);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(20, 15);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "ID";
+
+            // txtId
+            this.txtId.Location = new System.Drawing.Point(120, 27);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(120, 23);
+            this.txtId.TabIndex = 1;
+
+            // lblNombre
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(30, 70);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(56, 15);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre";
+
+            // txtNombre
+            this.txtNombre.Location = new System.Drawing.Point(120, 67);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(250, 23);
+            this.txtNombre.TabIndex = 3;
+
+            // lblEmail
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(30, 110);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(36, 15);
+            this.lblEmail.TabIndex = 4;
+            this.lblEmail.Text = "Email";
+
+            // txtEmail
+            this.txtEmail.Location = new System.Drawing.Point(120, 107);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(250, 23);
+            this.txtEmail.TabIndex = 5;
+
+            // lblTelefono
+            this.lblTelefono.AutoSize = true;
+            this.lblTelefono.Location = new System.Drawing.Point(30, 150);
+            this.lblTelefono.Name = "lblTelefono";
+            this.lblTelefono.Size = new System.Drawing.Size(58, 15);
+            this.lblTelefono.TabIndex = 6;
+            this.lblTelefono.Text = "Teléfono";
+
+            // txtTelefono
+            this.txtTelefono.Location = new System.Drawing.Point(120, 147);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(250, 23);
+            this.txtTelefono.TabIndex = 7;
+
+            // btnGuardar
+            this.btnGuardar.Location = new System.Drawing.Point(30, 200);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 35);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+
+            // btnBuscar
+            this.btnBuscar.Location = new System.Drawing.Point(140, 200);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(100, 35);
+            this.btnBuscar.TabIndex = 9;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+
+            // btnActualizar
+            this.btnActualizar.Location = new System.Drawing.Point(250, 200);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(100, 35);
+            this.btnActualizar.TabIndex = 10;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+
+            // btnEliminar
+            this.btnEliminar.Location = new System.Drawing.Point(360, 200);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 35);
+            this.btnEliminar.TabIndex = 11;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+
+            // dgvClientes
+            this.dgvClientes.ColumnHeadersHeightSizeMode =
+                System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+
+            this.dgvClientes.Location = new System.Drawing.Point(30, 260);
+            this.dgvClientes.Name = "dgvClientes";
+            this.dgvClientes.RowTemplate.Height = 25;
+            this.dgvClientes.Size = new System.Drawing.Size(740, 220);
+            this.dgvClientes.TabIndex = 12;
+
+            // Form1
+            this.AutoScaleDimensions =
+                new System.Drawing.SizeF(7F, 15F);
+
+            this.AutoScaleMode =
+                System.Windows.Forms.AutoScaleMode.Font;
+
+            this.ClientSize =
+                new System.Drawing.Size(800, 520);
+
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.txtId);
+
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.txtNombre);
+
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtEmail);
+
+            this.Controls.Add(this.lblTelefono);
+            this.Controls.Add(this.txtTelefono);
+
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnEliminar);
+
+            this.Controls.Add(this.dgvClientes);
+
+            this.Name = "Form1";
+            this.StartPosition =
+                System.Windows.Forms.FormStartPosition.CenterScreen;
+
+            this.Text = "CRUD Oracle 19c";
+
+            ((System.ComponentModel.ISupportInitialize)(this.dgvClientes)).EndInit();
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
     }
-}
+}   
