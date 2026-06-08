ff91b6c8e8787abc27f19453453a96ac931a748c
gtapiero <giovanny.tapiero@pi.edu.co>
2026-06-08 10:51:23 -0500

    commit - ConexionOracle / .

---
+++ ConexionOracle.cs
@@ -0,0 +1,22 @@
+﻿using Oracle.ManagedDataAccess.Client;
+
+namespace crud_oracle_19c
+{
+    public class ConexionOracle
+    {
+        private string cadena =
+            "User Id= SYSTEM;" +
+            "Password= Tapiero123;" +
+            "Data Source= localhost:1521/orcl;";
+
+        public OracleConnection ObtenerConexion()
+        {
+            OracleConnection cn =
+                new OracleConnection(cadena);
+
+            cn.Open();
+
+            return cn;
+        }
+    }   
+}
